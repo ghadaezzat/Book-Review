1c5b70b32c97a67178d46dd5d60a2546e25c28ba
ghadaezzat <ghadaezzat89@gmail.com>
2016-05-01 11:20:30 +0200

    filter books by categories

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
 class BooksController < ApplicationController
   before_action :find_book, only: [:show, :edit, :update, :destroy]
   def index
-    @books = Book.all.order("created_at DESC")
+    if params[:category].nil?
+      @books = Book.all.order("created_at DESC")
+    else
+      @category_id=Category.find_by(name: params[:category]).id
+      @books=Book.where( :category_id => @category_id).order("created_at DESC")
+    end
   end
   
   def show
@@ -22,9 +27,10 @@ class BooksController < ApplicationController
     end
   end
   def edit
-    
+       @categories=Category.all.map{ |c| [c.name,c.id]}
   end
   def update
+    @book.category_id=params[:category_id]
     if @book.update(book_params)
       redirect_to book_path(@book) 
     else
@@ -40,7 +46,7 @@ class BooksController < ApplicationController
   
   
   def book_params
-    params.require(:book).permit(:title,:description,:author)
+    params.require(:book).permit(:title,:description,:author,:category_id)
    
   end
   def find_book
