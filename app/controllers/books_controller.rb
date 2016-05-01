ac10c9d8a7c88d4b16373362b31a155948c2ee40
ghadaezzat <ghadaezzat89@gmail.com>
2016-05-01 12:26:06 +0200

    adding pictures to books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -46,7 +46,7 @@ class BooksController < ApplicationController
   
   
   def book_params
-    params.require(:book).permit(:title,:description,:author,:category_id)
+    params.require(:book).permit(:title,:description,:author,:category_id ,:book_img)
    
   end
   def find_book
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160430214126) do
+ActiveRecord::Schema.define(version: 20160501100248) do
 
   create_table "books", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.text     "description", limit: 65535
-    t.string   "author",      limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "user_id",     limit: 4
-    t.integer  "category_id", limit: 4
+    t.string   "title",                 limit: 255
+    t.text     "description",           limit: 65535
+    t.string   "author",                limit: 255
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.integer  "user_id",               limit: 4
+    t.integer  "category_id",           limit: 4
+    t.string   "book_img_file_name",    limit: 255
+    t.string   "book_img_content_type", limit: 255
+    t.integer  "book_img_file_size",    limit: 4
+    t.datetime "book_img_updated_at"
   end
 
   create_table "categories", force: :cascade do |t|
